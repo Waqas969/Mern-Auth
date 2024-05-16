434a7f1bb8eee9836d3f33b42a27e15bc0809997
waqas <waqas342@gamil.com>
2024-05-15 17:34:19 -0700

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Route, Router } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
 import Signin from "./Pages/Signin";
 import SignUp from "./Pages/SignUp";
+import Header from "./component/Header";
 function App() {
   return (
     <BrowserRouter>
-      <Router>
+      <Header />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<SignUp />} />
-      </Router>
+      </Routes>
     </BrowserRouter>
   );
 }
